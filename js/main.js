b909b557ac83dbabdbee94dcbf39d97faad748ca
ToleonPrins <icloud13@mail.ru>
2022-10-06 14:48:55 +0700

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 // Функция поиска случайного целого числа из диапазона
+
 function getRandomIntInclusive(a, b) {
   if (a < 0 || b < 0) {
     return NaN;
   }
-    const lower = Math.ceil(Math.min(a, b)); //Поиск минимального числа среди переданных аргументов и его округление
-    const upper = Math.floor(Math.max(a, b)); //Поиск максимального числа среди переданных аргументов и его округление
-    const result = Math.random() * (upper - lower + 1) + lower; //Поиск случайного целого числа в заданном диапазоне
+  const lower = Math.ceil(Math.min(a, b)); //Поиск минимального числа среди переданных аргументов и его округление
+  const upper = Math.floor(Math.max(a, b)); //Поиск максимального числа среди переданных аргументов и его округление
+  const result = Math.random() * (upper - lower + 1) + lower; //Поиск случайного целого числа в заданном диапазоне
 
-    return Math.floor(result);
-  }
+  return Math.floor(result);
+}
 
 // Функция проверки максимальной длины строки
 function getMaxLenghtText(checkText, maxLenghtText) {
   if (checkText.length <= maxLenghtText) {
     return true;
   }
-    return false;
-  }
+  return false;
+}
 
 // Вызов функций
 getRandomIntInclusive(44, 11);
