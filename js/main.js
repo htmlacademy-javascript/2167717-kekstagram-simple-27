0cd3c6ea8775e4c2b29d157d4598f74fc827abf6
ToleonPrins <icloud13@mail.ru>
2022-10-13 19:32:16 +0700

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,24 +1,78 @@
-// Функция поиска случайного целого числа из диапазона
+/**
+ * Получение случайного числа в диапазоне
+ * @param {Number|String} firstNumber - первое число
+ * @param {Number|String} secondNumber - второе число
+ * @returns {Number|String} вернется случайное число
+ */
 
-function getRandomIntInclusive(a, b) {
-  if (a < 0 || b < 0) {
+ const getRandomIntInclusive = (firstNumber, secondNumber) => {
+  if (firstNumber < 0 || secondNumber < 0) { //Сравнение чисел. Вернется NaN если одно или оба указанных числа отрицательные
     return NaN;
   }
-  const lower = Math.ceil(Math.min(a, b)); //Поиск минимального числа среди переданных аргументов и его округление
-  const upper = Math.floor(Math.max(a, b)); //Поиск максимального числа среди переданных аргументов и его округление
-  const result = Math.random() * (upper - lower + 1) + lower; //Поиск случайного целого числа в заданном диапазоне
+  const lowestNumber = Math.ceil(Math.min(firstNumber, secondNumber)); //Получение наименьшего целого числа среди переданных аргументов
+  const greatestNumber = Math.floor(Math.max(firstNumber, secondNumber)); //Получение наибольшего целого числа среди переданных аргументов
+  const randomResult = Math.random() * (greatestNumber - lowestNumber + 1) + lowestNumber; //Получение случайного целого числа в заданном диапазона
 
-  return Math.floor(result);
+  return Math.floor(randomResult);
 }
 
-// Функция проверки максимальной длины строки
-function getMaxLenghtText(checkText, maxLenghtText) {
-  if (checkText.length <= maxLenghtText) {
-    return true;
-  }
-  return false;
-}
-
-// Вызов функций
 getRandomIntInclusive(44, 11);
-getMaxLenghtText('Some text', 7);
+
+
+/**
+ * Проверка максимальной длины строки
+ * @param {String|Number} checkText - проверяемая строка
+ * @param {String|Number} maxLengthText - максимальная длина строки
+ * @returns {boolean} вернется проверка длины строки
+ */
+
+const getMaxLengthText = (checkText, maxLengthText) => {
+  if (checkText.length <= maxLengthText) { //Сравнение строки и максимальной длины этой строки
+    return true; //Длина строки в допустимом диапазоне
+  }
+  return false; //Длина строки больше допустимого диапазона
+}
+
+getMaxLengthText('Some text', 7);
+
+
+/**
+ * @returns {string}  вернется случайное значение из массива в виде строки
+ */
+const PHOTOS_DESCRIPTION = [//Массив с описаниями к фото
+  'В рестике', 'кодим', 'с собакой', 'с котом', 'делаю сайт', 'до дома пешком',
+  'Сложно учиться', 'Учу Java Script', 'Это мой первый программный язык',
+  'Я отдыхаю', 'гамаю', 'лежу', 'читаю', 'страдаю', 'в точку смотрю',
+  'На улице осень', 'грядут холода', 'Расчехлил сноуборд', 'зима близко', 'друзья',
+  'Сейчас бы на пляж, а не вот это вот все', 'вчера на машине проколол колесо',
+  'Нужно быть сильным', 'Кекса можно простить', ' заставил меня слишком много учить',
+]
+
+const getRandomDescription = () =>{
+  const randomDescription = Math.floor(Math.random() * PHOTOS_DESCRIPTION.length); //Получение случайного элемента массива
+  return PHOTOS_DESCRIPTION[randomDescription];
+}
+getRandomDescription();
+
+
+/**
+ * Создание массива из сгенерированных объектов
+ * @param {Number} count - количество генерируемых объектов в созданном массиве
+ * @returns {Array} вернется массив объектов
+ */
+const createPictureAttribute = (count) => {
+  const pictureAttribute = []; //Создаем пустой массив
+  for (let i = 1; i <= count; i++) {
+    pictureAttribute[i - 1] = {
+      id: i,
+      url:  `photos/${i}.jpg`,
+      description: getRandomDescription(),
+      likes: getRandomIntInclusive(15, 200),
+      comments: getRandomIntInclusive(0, 200),
+    }
+  }
+  return pictureAttribute;
+}
+
+const objectOfPhotos = createPictureAttribute(25);
+console.table(objectOfPhotos);
